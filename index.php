a1d051b9ccfe4ae696156c5c23d9e779dc752223
Robert Carr <rob@cloudscaling.com>
2013-05-08 16:37:59 -0700

    Added logo

--- index.php
+++ index.php
@@ -4,21 +4,19 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>OCS Sample Site</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>OCS Demo Page</h1>
                 <h2>Congratulations!</h2>
-                <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
-                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+                <IMG SRC="logo.png" align=center>
             </div>
         </div>
 
